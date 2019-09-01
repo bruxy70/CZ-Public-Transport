98033192e7579d9b9ecc668d9b99eaec69a2b25a
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2019-09-01 10:24:35 +0200

    Update sensor.py

--- custom_components/cz_pub_tran/sensor.py
+++ custom_components/cz_pub_tran/sensor.py
@@ -1,15 +1,15 @@
 """Support for cz_pub_tran sensors."""
 import logging, json, requests
-from requests import get
 from datetime import datetime, date, time, timedelta
-
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import async_timeout
 import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
     CONF_NAME
 )
 from homeassistant.core import HomeAssistant, State
-import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
@@ -42,8 +42,15 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 
 SCAN_INTERVAL = timedelta(seconds=60)
 THROTTLE_INTERVAL = timedelta(seconds=60)
+HTTP_TIMEOUT = 5
 
 TRACKABLE_DOMAINS = ["sensor"]
+
+class ErrorGettingData(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Setup the sensor platform."""
@@ -53,6 +60,7 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     combination_id = config.get(CONF_COMBINATION_ID)
     user_id = config.get(CONF_USERID)
     async_add_entities([CZPubTranSensor(name, origin, destination,combination_id,user_id)],True)
+
 
 class CZPubTranSensor(Entity):
     """Representation of a openroute service travel time sensor."""
@@ -118,40 +126,38 @@ class CZPubTranSensor(Entity):
 
     async def async_update_CombinationInfo(self):
         url_combination  = 'https://ext.crws.cz/api/'
-        combination_decoded = {}
         _LOGGER.info( "(" + self._name + ") Updating CombinationInfo guid")
         self._combination_guid = None
         self._guid_valid_to = None
         if self._user_id=="":
-            try:
-                combination_response = get(url_combination)
-                _LOGGER.debug( "(" + self._name + ") url - " + combination_response.url)
-            except:
-                _LOGGER.info( "(" + self._name + ") Exception reading guid data")
+            payload = {}
         else:
             payload= {'userId':self._user_id}
-            try:
-                combination_response = get(url_combination,params=payload)
-                _LOGGER.debug( "(" + self._name + ") url - " + combination_response.url)
-            except:
-                _LOGGER.info( "(" + self._name + ") Exception reading guid data")
-
-        if combination_response.status_code == 200:
-            combination_json_input = combination_response.text
-            combination_decoded = json.loads(combination_json_input)
-            if "data" in combination_decoded:
-                try:
-                    for combination in combination_decoded["data"]:
-                        if combination["id"] == self._combination_id:
-                            self._combination_guid = combination["guid"]
-                            self._guid_valid_to = datetime.strptime(combination["ttValidTo"], "%d.%m.%Y").date()
-                            _LOGGER.debug( "(" + self._name + ") found guid - " + self._combination_guid +" valid to "+self._guid_valid_to.strftime("%d-%m-%Y"))
-                except:
-                    _LOGGER.error( "(" + self._name + ") Exception parsing guid")
-            else:
-                _LOGGER.error( "(" + self._name + ") Reading guid - API returned no data")
-        else:
-            _LOGGER.error( "(" + self._name + ") Reading guid - API returned code " + str(combination_response.status_code))
+        session = async_get_clientsession(self.hass)
+        self._combination_guid = None
+        self._guid_valid_to = None
+        try:
+            with async_timeout.timeout(HTTP_TIMEOUT):            
+                combination_response = await session.get(url_combination,params=payload)
+            if combination_response is None:
+                raise ErrorGettingData('No response')
+            _LOGGER.debug( "(" + self._name + ") url - " + str(combination_response.url))
+            if combination_response.status != 200:
+                raise ErrorGettingData('API returned response code '+str(combination_response.status)+" ("+await combination_response.text()+")" )
+            combination_decoded = await combination_response.json()
+            if combination_decoded is None:
+                raise ErrorGettingData('Error passing the JSON response')
+            if "data" not in combination_decoded:
+                raise ErrorGettingData('API returned no data')
+            for combination in combination_decoded["data"]:
+                if combination["id"] == self._combination_id:
+                    self._combination_guid = combination["guid"]
+                    self._guid_valid_to = datetime.strptime(combination["ttValidTo"], "%d.%m.%Y").date()
+                    _LOGGER.debug( "(" + self._name + ") found guid - " + self._combination_guid +" valid till "+self._guid_valid_to.strftime("%d-%m-%Y"))
+        except ErrorGettingData as e:
+            _LOGGER.error( "(" + self._name + ") Error getting CombinatonInfo: "+ e.value)
+        except:
+            _LOGGER.error( "(" + self._name + ") Exception reading guid data")
         if self._combination_guid is None:
             self._state = ""
             self._duration = ""
@@ -162,67 +168,64 @@ class CZPubTranSensor(Entity):
     async def async_update_Connection(self):
         if self._combination_guid is None:
             return
-        url_connection = "https://ext.crws.cz/api/"+self._combination_guid+"/connections"
+        url_connection = "https://ext.crws.cz/api/"+self._combination_guid+"/connections"        
         if self._user_id=="":
             payload= {'from':self._origin, 'to':self._destination}
         else:
             payload= {'from':self._origin, 'to':self._destination,'userId':self._user_id}
         _LOGGER.info( "(" + self._name + ") Checking connection from "+ self._origin+" to "+self._destination)            
+        session = async_get_clientsession(self.hass)
+        self._combination_guid = None
+        self._guid_valid_to = None
         try:
-            connection_response = get(url_connection,params=payload)
-            _LOGGER.debug( "(" + self._name + ") url - " + connection_response.url)
+            with async_timeout.timeout(HTTP_TIMEOUT):            
+                connection_response = await session.get(url_connection,params=payload)
+            if connection_response is None:
+                raise ErrorGettingData('No response')
+            _LOGGER.debug( "(" + self._name + ") url - " + str(connection_response.url))
+            if connection_response.status != 200:
+                raise ErrorGettingData('API returned response code '+str(connection_response.status)+" ("+await connection_response.text()+")")
+            connection_decoded = await connection_response.json()
+            if connection_decoded is None:
+                raise ErrorGettingData('Error passing the JSON response')
+            if "handle" not in connection_decoded:
+                raise ErrorGettingData('Did not find any connection from '+self._origin+" to "+self._destination)
+
+            connection = connection_decoded["connInfo"]["connections"][0]
+            _LOGGER.info( "(" + self._name + ") connection from "+self._origin+" to "+self._destination+ ": id"+str(connection["id"]))
+            self._duration = connection["timeLength"]
+            self._departure = connection["trains"][0]["trainData"]["route"][0]["depTime"]
+            connections_short=""
+            connections_long=""
+            first=True
+            for trains in connection["trains"]:
+                line=str(trains["trainData"]["info"]["num1"])
+                depTime=trains["trainData"]["route"][0]["depTime"]
+                depStation=trains["trainData"]["route"][0]["station"]["name"]
+                if "arrTime" in trains["trainData"]["route"][1]:
+                    arrTime=trains["trainData"]["route"][1]["arrTime"]
+                else:
+                    arrTime=trains["trainData"]["route"][1]["depTime"]
+                arrStation=trains["trainData"]["route"][1]["station"]["name"]
+                if first:
+                    connections_short=line
+                    connections_long=line+" "+depTime+" ("+depStation+") -> "+arrTime+" ("+arrStation+")"
+                    first=False
+                else:
+                    connections_short=connections_short+"-"+depStation.replace(" (PZ)","")+"-"+line
+                    connections_long=connections_long+"\n"+line+" "+depTime+" ("+depStation+") -> "+arrTime+" ("+arrStation+")"
+            self._state = self._departure+" ("+connections_short+")"
+            self._connections = connections_short
+            self._description = connections_long
+        except ErrorGettingData as e:
+            _LOGGER.error( "(" + self._name + ") Error getting connection: "+ e.value)
+            self._state = ""
+            self._duration = ""
+            self._departure = ""
+            self._connections = ""
+            self._description = ""
         except:
-            _LOGGER.error( "(" + self._name + ") Exception reading connection data")
-
-        if connection_response.status_code == 200:
-            connection_json_input = connection_response.text
-            # _LOGGER.debug( "(" + self._name + ") response - " + connection_json_input)
-            connection_decoded = json.loads(connection_json_input)
-
-            if "handle" in connection_decoded:
-                try:
-                    connection = connection_decoded["connInfo"]["connections"][0]
-                    _LOGGER.info( "(" + self._name + ") connection from "+self._origin+" to "+self._destination+ ": id"+str(connection["id"]))
-                    self._duration = connection["timeLength"]
-                    self._departure = connection["trains"][0]["trainData"]["route"][0]["depTime"]
-                    connections_short=""
-                    connections_long=""
-                    first=True
-                    for trains in connection["trains"]:
-                        line=str(trains["trainData"]["info"]["num1"])
-                        depTime=trains["trainData"]["route"][0]["depTime"]
-                        depStation=trains["trainData"]["route"][0]["station"]["name"]
-                        if "arrTime" in trains["trainData"]["route"][1]:
-                            arrTime=trains["trainData"]["route"][1]["arrTime"]
-                        else:
-                            arrTime=trains["trainData"]["route"][1]["depTime"]
-                        arrStation=trains["trainData"]["route"][1]["station"]["name"]
-                        if first:
-                            connections_short=line
-                            connections_long=line+" "+depTime+" ("+depStation+") -> "+arrTime+" ("+arrStation+")"
-                            first=False
-                        else:
-                            connections_short=connections_short+"-"+depStation.replace(" (PZ)","")+"-"+line
-                            connections_long=connections_long+"\n"+line+" "+depTime+" ("+depStation+") -> "+arrTime+" ("+arrStation+")"
-                    self._state = self._departure+" ("+connections_short+")"
-                    self._connections = connections_short
-                    self._description = connections_long
-                except:
-                    _LOGGER.error( "(" + self._name + ") Exception parsing connecton")
-                    self._state = ""
-                    self._duration = ""
-                    self._departure = ""
-                    self._connections = ""
-                    self._description = ""
-            else:
-                _LOGGER.error( "(" + self._name + ") connection from "+self._origin+" to "+self._destination+ " not found!")
-                self._state = ""
-                self._duration = ""
-                self._departure = ""
-                self._connections = ""
-                self._description = ""
-        else:
-            _LOGGER.error( "(" + self._name + ") Reading connection data - API returned code " + str(connection_response.status_code))
+            _LOGGER.error( "(" + self._name + ") Exception getting connection data")
             self._state = ""
             self._duration = ""
             self._departure = ""
