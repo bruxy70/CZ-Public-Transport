16f2841bb98652156490aab54be3ab2240844395
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2019-09-02 09:03:07 +0200

    Update __init__.py

--- custom_components/cz_pub_tran/__init__.py
+++ custom_components/cz_pub_tran/__init__.py
@@ -1,1 +1,19 @@
+"""Support for cz_pub_tran domain"""
+import logging, json, requests
+from datetime import datetime, date, time, timedelta
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import async_timeout
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.util import Throttle
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.core import HomeAssistant, State
+from homeassistant.helpers.entity import Entity
 
+_LOGGER = logging.getLogger(__name__)
+DOMAIN = 'cz_pub_tran'
+
+async def async_setup(hass, base_config):
+    """Setup the sensor platform."""
+    hass.data[DOMAIN] = False
+    return True
