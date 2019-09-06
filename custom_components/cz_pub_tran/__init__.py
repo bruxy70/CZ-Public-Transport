c3b6778a4dfb452c6899a09306bdeb0ae0467000
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2019-09-06 21:43:04 +0200

    In Progress

--- custom_components/cz_pub_tran/__init__.py
+++ custom_components/cz_pub_tran/__init__.py
@@ -4,137 +4,93 @@ If the connection is scheduled, it skips the update.
 But every 5 minutes it updates all connections regardless - to check on delay
 """
 
+from . import (
+    CZPubTranSensor,
+    CONF_ORIGIN,
+    CONF_DESTINATION,
+    CONF_USERID,
+    CONF_COMBINATION_ID,
+)
+from homeassistant.const import (
+    CONF_SCAN_INTERVAL,
+    CONF_SENSORS,
+    CONF_NAME
+)
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import PLATFORM_SCHEMA
 import logging, json, requests
 from datetime import datetime, date, time, timedelta
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
-import async_timeout
 import asyncio
 from homeassistant.core import HomeAssistant, State
 from homeassistant.helpers.entity import Entity, async_generate_entity_id
 from homeassistant.helpers.event import async_call_later
 
 _LOGGER = logging.getLogger(__name__)
-DOMAIN = 'sensor'
-SCAN_INTERVAL = 60
-ENTITY_ID_FORMAT = DOMAIN + ".{}"
+DOMAIN = 'cz_pub_tran'
+COMPONENT_NAME = 'sensor'
+DEFAULT_SCAN_INTERVAL = timedelta(seconds=60)
+DEFAULT_COMBINATION_ID = "ABCz"
+DEFAULT_NAME = "cz_pub_tran"
+
+ENTITY_ID_FORMAT = COMPONENT_NAME + ".{}"
 FORCED_REFRESH_COUNT = 5
 HTTP_TIMEOUT = 5
 
-async def async_setup(hass, base_config):
+SENSOR_SCHEMA = vol.Schema({
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Required(CONF_ORIGIN): cv.string,
+    vol.Required(CONF_DESTINATION): cv.string,
+    vol.Optional(CONF_COMBINATION_ID, default=DEFAULT_COMBINATION_ID): cv.string,
+})
+
+CONFIG_SCHEMA = vol.Schema({
+    DOMAIN: vol.Schema({
+        vol.Optional(CONF_USERID,default=""): cv.string,
+        vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): cv.time_period,
+        vol.Optional(CONF_SENSORS, default={}): SENSOR_SCHEMA,
+    })
+})
+
+async def async_setup(hass, config):
     """Setup the sensor platform."""
-    Connection.session = async_get_clientsession(hass)
-    async_call_later(hass,1, Connection.async_update_Connections())
+    user_id = config.get(CONF_USERID)
+    scan_interval = config.get(CONF_SCAN_INTERVAL).total_seconds()
+    hass.data[DOMAIN]= ConnectionPlatform(user_id,scan_interval,session)
+
+    discovery.load_platform(hass, COMPONENT_NAME, DOMAIN, conf[CONF_SENSORS], config)
+    async_call_later(hass,1, hass.data[DOMAIN].async_update_Connections())
     return True
 
-class CombinationID():
-    combination_ids = {}
-    def __init__(self,guid,validTo):
-        self.validTo = validTo
-        self.guid=guid
-    
-    @staticmethod
-    def guid_exist(combination_id):
-        """Return False if Combination ID needs to be updated."""
-        try:
-            if combination_id in CombinationID.combination_ids:
-                today=datetime.now().date()
-                if CombinationID.combination_ids[combination_id].validTo >= today:
-                    return True
-                else:
-                    return False
-            else:
-                return False
-        except:
-            return False # Refresh data on Error
-
-
-    @staticmethod
-    def get_guid(combination_id):
-        if combination_id in CombinationID.combination_ids:
-            return CombinationID.combination_ids[combination_id].guid
-        else:
-            _LOGGER.error(f'GUID for combination ID {combination_id} not found!')
-            return ''
-
-    @staticmethod
-    def clean_combination_ids(combination_id):
-        if combination_id in CombinationID.combination_ids:
-            del CombinationID[combination_id]
-
-class ErrorGettingData(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
-
-class Connection(Entity):
-    """Representation of a device entity. Will pass to binary_sensor and others"""
-    entity_ids = []
-    connections = []
-    session = None
-    hass = None
-
-    def scheduled_connection(self):
-        """Return False if Connection needs to be updated."""
-        try:
-            if self._departure == "" or self.forced_refresh_countdown <= 0:
-                self.forced_refresh_countdown = FORCED_REFRESH_COUNT
-                return False
-            departure_time=datetime.strptime(self._departure,"%H:%M").time()
-            now=datetime.now().time()
-            if now < departure_time or ( now.hour> 22 and departure_time < 6 ):
-                self.forced_refresh_countdown = self.forced_refresh_countdown - 1
-                return True
-            else:
-                self.forced_refresh_countdown = FORCED_REFRESH_COUNT
-                return False
-        except:
-            return False # Refresh data on Error
-
-
-    def __init__(self,hass,session, name, origin, destination,combination_id,user_id):
-        """Initialize the device."""
-        Connection.hass = hass
-        self._session = session
-        self._name = name
-        self._origin = origin
-        self._destination = destination
-        self._combination_id = combination_id
+class ConnectionPlatform(Entity):
+    def __init__(self,user_id,scan_interval,session):
         self._user_id = user_id
-        self._lastupdated = None
-        self._forced_refresh_countdown = 0
-        self.load_defaults()
-        self.entity_id=async_generate_entity_id(ENTITY_ID_FORMAT,name,Connection.entity_ids)
-        Connection.entity_ids.append(self.entity_id)
-        _LOGGER.debug(f'Entity {self.entity_id} inicialized')
-    
-    def load_defaults(self):
-        self._state = ""
-        self._delay = ""
-        self._duration = ""
-        self._departure = ""
-        self._connections = ""
-        self._description = ""
-        
-    async def async_added_to_hass(self):
-        """I probably do not need this! To be removed! Call when entity is added to hass."""
-        Connection.connections.append(self)
-        _LOGGER.debug(f'Entity {self.entity_id} added')
+        self._scan_interval = scan_interval
+        self._session = session
+        self._entity_ids = []
+        self._connections = []
 
+    def add_sensor(self,sensor):
+        self._connections.append(self,sensor)
 
-    @staticmethod
-    async def async_update_CombinationInfo(combination_id,user_id):
+    def entity_ids(self):
+        return self._entity_ids
+
+    def add_entity_if(self,id):
+        self._entity_ids.append(id)
+
+    async def async_update_CombinationInfo(self,combination_id):
         if CombinationID.guid_exist(combination_id):
             return True
         url_combination  = 'https://ext.crws.cz/api/'
         _LOGGER.debug( f'Updating CombinationInfo guid {combination_id}')
-        if user_id=="":
+        if self._user_id=="":
             payload = {}
         else:
-            payload= {'userId':user_id}
+            payload= {'userId':self._user_id}
         try:
             with async_timeout.timeout(HTTP_TIMEOUT):            
-                combination_response = await Connection.session.get(url_combination,params=payload)
+                combination_response = await self._session.get(url_combination,params=payload)
             if combination_response is None:
                 raise ErrorGettingData('Response timeout')
             _LOGGER.debug( f'url - {combination_response.url}')
@@ -157,24 +113,23 @@ class Connection(Entity):
             _LOGGER.error( 'Exception reading guid data')
         return False
 
-    @staticmethod
-    async def async_update_Connections():
-        for entity in Connection.connections:
+    async def async_update_Connections(self):
+        for entity in self._connections:
             if not CombinationID.guid_exist(entity._combination_id):
-                if not await Connection.async_update_CombinationInfo(entity._combination_id,entity._user_id):
+                if not await Connection.async_update_CombinationInfo(entity._combination_id,self._user_id):
                     continue
             if entity.scheduled_connection():
                 _LOGGER.debug( f'({entity._name}) departure already scheduled for {entity._departure} - not checking connections')
                 continue
             url_connection = f'https://ext.crws.cz/api/{CombinationID.get_guid(entity._combination_id)}/connections'
-            if entity._user_id=="":
+            if self._user_id=="":
                 payload= {'from':entity._origin, 'to':entity._destination}
             else:
-                payload= {'from':entity._origin, 'to':entity._destination,'userId':entity._user_id}
+                payload= {'from':entity._origin, 'to':entity._destination,'userId':self._user_id}
             _LOGGER.debug( f'({entity._name}) Checking connection from {entity._origin} to {entity._destination}')            
             try:
                 with async_timeout.timeout(HTTP_TIMEOUT):            
-                    connection_response = await Connection.session.get(url_connection,params=payload)
+                    connection_response = await self._session.get(url_connection,params=payload)
 
                 if connection_response is None:
                     raise ErrorGettingData('Response timeout')
@@ -189,8 +144,8 @@ class Connection(Entity):
 
                 connection = connection_decoded["connInfo"]["connections"][0]
                 _LOGGER.debug( f"({entity._name}) connection from {entity._origin} to {entity._destination}: found id {str(connection['id'])}")
-                entity._duration = connection["timeLength"]
-                entity._departure = connection["trains"][0]["trainData"]["route"][0]["depTime"]
+                duration = connection["timeLength"]
+                departure = connection["trains"][0]["trainData"]["route"][0]["depTime"]
                 connections_short=''
                 connections_long=''
                 delay=''
@@ -217,14 +172,17 @@ class Connection(Entity):
                     else:
                         connections_long=connections_long+long_delim+f'{line:<4} {depTime:<5} ({depStation}) -> {arrTime:<5} ({arrStation})'
                     long_delim='\n'
-                entity._state = entity._departure+" ("+connections_short+")"
-                entity._connections = connections_short
-                entity._description = connections_long
-                entity._delay = delay
+                entity.update_status(departure,duration,departure+" ("+connections_short+")",connections_short,connections_long,delay)
             except ErrorGettingData as e:
                 _LOGGER.error( f'({entity._name}) Error getting connection: {e.value}')
                 entity.load_defaults()
             except:
                 _LOGGER.error( f'({entity._name}) Exception getting connection data')
                 entity.load_defaults()
-        async_call_later(Connection.hass, SCAN_INTERVAL, Connection.async_update_Connections())
+        async_call_later(self.hass, self._scan_interval, self.async_update_Connections())
+
+class ErrorGettingData(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
--- custom_components/cz_pub_tran/sensor.py
+++ custom_components/cz_pub_tran/sensor.py
@@ -3,9 +3,6 @@ from . import Connection
 import logging, json, requests
 from datetime import datetime, date, time, timedelta
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-import voluptuous as vol
-import homeassistant.helpers.config_validation as cv
-from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
     CONF_NAME
 )
@@ -14,8 +11,6 @@ from homeassistant.helpers.entity import Entity
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "cz_pub_tran"
-
 ICON_BUS = "mdi:bus"
 
 CONF_ORIGIN = "origin"
@@ -23,7 +18,6 @@ CONF_DESTINATION = "destination"
 CONF_USERID = "userId"
 
 CONF_COMBINATION_ID = "combination_id"
-DEFAULT_COMBINATION_ID = "ABCz"
 
 ATTR_DURATION = "duration"
 ATTR_DEPARTURE = "departure"
@@ -31,25 +25,7 @@ ATTR_CONNECTIONS = "connections"
 ATTR_DESCRIPTION = "description"
 ATTR_DELAY = "delay"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_ORIGIN): cv.string,
-    vol.Required(CONF_DESTINATION): cv.string,
-    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
-    vol.Optional(CONF_COMBINATION_ID, default=DEFAULT_COMBINATION_ID): cv.string,
-    vol.Optional(CONF_USERID,default=""): cv.string,
-})
-
-SCAN_INTERVAL = timedelta(seconds=60)
-THROTTLE_INTERVAL = timedelta(seconds=10)
-HTTP_TIMEOUT = 5
-
 TRACKABLE_DOMAINS = ["sensor"]
-
-class ErrorGettingData(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Setup the sensor platform."""
@@ -57,16 +33,23 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
     destination = config[CONF_DESTINATION]
     name = config.get(CONF_NAME)
     combination_id = config.get(CONF_COMBINATION_ID)
-    user_id = config.get(CONF_USERID)
-    session = async_get_clientsession(hass)
-    async_add_entities([CZPubTranSensor(hass, session, name, origin, destination,combination_id,user_id)],True)
+    async_add_entities([CZPubTranSensor(hass, name, origin, destination,combination_id)],True)
 
 
-class CZPubTranSensor(Connection):
+class CZPubTranSensor(Entity):
     """Representation of a openroute service travel time sensor."""
-    def __init__(self, hass, session, name, origin, destination,combination_id,user_id):
+    def __init__(self, hass, session, name, origin, destination,combination_id):
         """Initialize the sensor."""
-        super().__init__(hass,session, name, origin, destination,combination_id,user_id)
+        self._name = name
+        self._origin = origin
+        self._destination = destination
+        self._combination_id = combination_id
+        self._lastupdated = None
+        self._forced_refresh_countdown = 0
+        self.load_defaults()
+        self.entity_id=async_generate_entity_id(ENTITY_ID_FORMAT,name,hass.data[DOMAIN].entity_ids())
+        hass.data[DOMAIN].add_entity_id(self.entity_id)
+        _LOGGER.debug(f'Entity {self.entity_id} inicialized')
 
     @property
     def name(self):
@@ -92,3 +75,36 @@ class CZPubTranSensor(Connection):
     @property
     def icon(self):
         return ICON_BUS
+
+    def scheduled_connection(self):
+        """Return False if Connection needs to be updated."""
+        try:
+            if self.forced_refresh_countdown <= 0 or self._departure == "":
+                self.forced_refresh_countdown = FORCED_REFRESH_COUNT
+                return False
+            departure_time=datetime.strptime(self._departure,"%H:%M").time()
+            now=datetime.now().time()
+            if now < departure_time or ( now.hour> 22 and departure_time < 6 ):
+                self.forced_refresh_countdown = self.forced_refresh_countdown - 1
+                return True
+            else:
+                self.forced_refresh_countdown = FORCED_REFRESH_COUNT
+                return False
+        except:
+            return False # Refresh data on Error
+
+    def update_status(self,departure,duration,state,connections,description,delay):
+        self._departure = departure
+        self._duration = duration
+        self._state = state
+        self._connections = connectiobs
+        self._description = description
+        self._delay = delay
+
+    def load_defaults(self):
+        self.update_status("","","","","","")
+        
+    async def async_added_to_hass(self):
+        """I probably do not need this! To be removed! Call when entity is added to hass."""
+        hass.data[DOMAIN].add_sensor(self)
+        _LOGGER.debug(f'Entity {self.entity_id} added')
