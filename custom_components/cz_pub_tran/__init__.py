0bcb33b535ce9353c979b48e11ac3b87871cf1fe
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2019-09-22 00:42:45 +0200

    Update __init__.py

--- custom_components/cz_pub_tran/__init__.py
+++ custom_components/cz_pub_tran/__init__.py
@@ -1,6 +1,6 @@
 """Support for cz_pub_tran domain
 The async_update connections checks all connections every minute
-If the connection is scheduled, it skips the update. 
+If the connection is scheduled, it skips the update.
 But every 5 minutes it updates all connections regardless - to check on delay
 """
 from czpubtran.api import czpubtran
@@ -31,7 +31,7 @@ from .sensor import (
     CONF_DESCRIPTION_FORMAT,
     CONFIG_SCHEMA,
     SET_START_TIME_SCHEMA,
-    ATTR_START_TIME,  
+    ATTR_START_TIME,
     COMPONENT_NAME
 )
 
@@ -45,13 +45,14 @@ async def async_setup(hass, config):
     user_id = conf.get(CONF_USERID)
     scan_interval = conf.get(CONF_SCAN_INTERVAL).total_seconds()
     force_refresh_period = conf.get(CONF_FORCE_REFRESH_PERIOD)
-    description_format=conf.get(CONF_DESCRIPTION_FORMAT)
+    description_format = conf.get(CONF_DESCRIPTION_FORMAT)
     session = async_get_clientsession(hass)
     hass.data[DOMAIN] = ConnectionPlatform(hass, user_id, scan_interval, force_refresh_period, description_format, session)
     hass.helpers.discovery.load_platform(COMPONENT_NAME, DOMAIN, conf[CONF_SENSORS], config)
     hass.services.async_register(DOMAIN, 'set_start_time', hass.data[DOMAIN].handle_set_time, schema=SET_START_TIME_SCHEMA)
     async_call_later(hass, 1, hass.data[DOMAIN].async_update_Connections())
     return True
+
 
 class ConnectionPlatform():
     def __init__(self, hass, user_id, scan_interval, force_refresh_period, description_format, session):
@@ -73,8 +74,8 @@ class ConnectionPlatform():
         else:
             _LOGGER.debug(f'Received call to set the start time in entity {_entity_id} to {_time}')
         entity = next((entity for entity in self._connections if entity.entity_id == _entity_id), None)
-        if entity is not None: 
-            entity._start_time=None if _time is None else _time.strftime("%H:%M")
+        if entity is not None:
+            entity._start_time = None if _time is None else _time.strftime("%H:%M")
             entity.load_defaults()
             async_call_later(self._hass, 0, self.async_update_Connections())
 
@@ -94,24 +95,37 @@ class ConnectionPlatform():
                 continue
             if await self._api.async_find_connection(entity._origin, entity._destination, entity._combination_id, entity._start_time):
                 description = DESCRIPTION_HEADER[self._description_format]
-                connections=''
-                delay=''
+                connections = ''
+                delay = ''
                 if self._api.connection_detail is not None and len(self._api.connection_detail) >= 1:
-                    for i,trains in enumerate(self._api.connection_detail[0]):
+                    for i, trains in enumerate(self._api.connection_detail[0]):
                         line = trains['line']
                         depTime = trains['depTime']
                         depStation = trains['depStation']
                         arrTime = trains['arrTime']
                         arrStation = trains['arrStation']
-                        depStationShort = "-"+depStation.replace(" (PZ)","")+"-"
+                        depStationShort = "-"+depStation.replace(" (PZ)", "") + "-"
                         connections += f'{depStationShort if i > 0 else ""}{line}'
                         if trains['delay'] != '':
-                            description += ('\n' if i > 0 else '') + DESCRIPTION_LINE_DELAY[self._description_format].format(line,depTime,depStation,arrTime,arrStation,trains["delay"])
+                            description += ('\n' if i > 0 else '') + DESCRIPTION_LINE_DELAY[self._description_format].format(
+                                    line,
+                                    depTime,
+                                    depStation,
+                                    arrTime,
+                                    arrStation,
+                                    trains["delay"]
+                            )
                             delay += f'{"" if delay=="" else " | "}line {line} - {trains["delay"]}min delay'
                         else:
-                            description += ('\n' if i > 0 else '') + DESCRIPTION_LINE_NO_DELAY[self._description_format].format(line,depTime,depStation,arrTime,arrStation)
+                            description += ('\n' if i > 0 else '') + DESCRIPTION_LINE_NO_DELAY[self._description_format].format(
+                                line,
+                                depTime,
+                                depStation,
+                                arrTime,
+                                arrStation
+                            )
                 description += DESCRIPTION_FOOTER[self._description_format]
                 entity.update_status(self._api.departure, self._api.duration, self._api.departure+" ("+connections+")", connections, description, self._api.connection_detail, delay)
             else:
-                entity.update_status('','',STATUS_NO_CONNECTION,'','',None,"")
+                entity.update_status('', '', STATUS_NO_CONNECTION, '', '', None, "")
         async_call_later(self._hass, self._scan_interval, self.async_update_Connections())
